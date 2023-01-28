d6ef3a0ccf54f833635f30ce74b1fc18008f3a56
default-writer <funcelot@outlook.com>
2023-01-28 15:24:37 +0300

    added cleanup script

---
+++ bin/cleanup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+err_report() {
+    echo "ERROR: on line $*: $(cat $0 | sed $1!d)" >&2
+}
+
+trap 'err_report $LINENO' ERR
+
+uid=$(id -u)
+
+if [ "${uid}" -eq 0 ]; then
+    echo "Please run as user"
+    exit
+fi
+
+pwd=$(pwd)
+
+install="$1"
+
+. "${pwd}/bin/scripts/load.sh"
+
+## Cleans up directories
+## Usage: ${script} <option>
+## ${commands}
+
+case "${install}" in
+
+    "--all") # cleans up uncommited changes and non-gited files and folders
+        ;;
+
+    *)
+        help    
+        ;;
+
+esac
+
+git clean -f -q -d -x
+
+[[ $SHLVL -gt 2 ]] || echo OK
+
+cd "${pwd}"
